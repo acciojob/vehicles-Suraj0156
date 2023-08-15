a8a7c2d1507114d6d78234d3c7ec362d10cbee24
suraj <surajhinge77@gmail.com>
2023-08-15 10:29:55 +0530

    hshshh

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -6,50 +6,39 @@ public class Vehicle {
     private int currentSpeed;
     private int currentDirection;
 
-    public String getName() {return name;
+    public Vehicle(String name) {
+        this.name=name;
+        currentSpeed=0;
+        currentDirection=0;
     }
 
-    public void setName(String name) {this.name = name;
-    }
-
-    public int getCurrentSpeed() {return currentSpeed;
-    }
-
-    public void setCurrentSpeed(int currentSpeed) {
-        this.currentSpeed = currentSpeed;
+    public String getName() {
+        return name;
     }
 
     public int getCurrentDirection() {
         return currentDirection;
     }
 
-    public void setCurrentDirection(int currentDirection) {
-        this.currentDirection = currentDirection;
+    public int getCurrentSpeed() {
+        return currentSpeed;
     }
 
-    public Vehicle(String name) {
-        this.name = name;
-        this.currentSpeed = 0;
-        this.currentDirection =0;
-
-  }
-
     public void steer(int direction){
-        this.currentDirection+=direction;
         //direction is in degrees, add it to the current direction
+        currentDirection+=direction;
         System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
     }
 
     public void move(int speed, int direction){
-        this.currentSpeed=speed;
-        this.currentDirection=direction;
         //set the values of currentSpeed and currentDirection
+        currentSpeed=speed;
+        currentDirection=direction;
         System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
     }
 
     public void stop(){
-        this.currentSpeed=0;
-
+        currentSpeed=0;
         System.out.println("stop method called - The vehicle is stopped");
     }
 
